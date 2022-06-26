b5c77025c42442fa321f37694f28b5cc40673ae0
Sophia Ye <sophiaye@Sophias-Air-3.hitronhub.home>
2022-06-25 19:03:50 -0700

    display image

--- frontend/src/components/GoalItem.jsx
+++ frontend/src/components/GoalItem.jsx
@@ -1,9 +1,12 @@
+import React from 'react';
 import {useDispatch} from 'react-redux'
 import {deleteGoal} from'../features/goals/goalSlice'
+import {Card, CardActions, CardContent, CardMedia, Button, Typography} from '@material-ui/core';
 
-function GoalItem({goal}) {
+// function GoalItem({goal}) {
+const GoalItem = ({goal}) =>{
   const dispatch = useDispatch()
-
+  // console.log(goal)
   return (
     <div className='goal'>
         <div>
@@ -11,6 +14,10 @@ function GoalItem({goal}) {
             ('en-US')}
         </div>
         <h2>{goal.text}</h2>
+        <Card>
+          <CardMedia component="img" image={goal.articleImage}/>
+        </Card>
+        
         <button onClick={() => dispatch (deleteGoal(goal._id))} className="close">X</button>
     </div>
   )
--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -5,6 +5,8 @@ import GoalForm from '../components/GoalForm'
 import GoalItem from '../components/GoalItem'
 import Spinner from '../components/Spinner'
 import { getGoals, reset } from '../features/goals/goalSlice'
+import {Grid, CircularProgress} from '@mui/material';
+
 
 function Dashboard() {
   const navigate = useNavigate()
@@ -34,24 +36,23 @@ function Dashboard() {
   }
 
   return <>
-  <section className="heading">
-    <h1>Welcome {user && user.name}</h1>
-    <p>Goals Dashboard</p>
-  </section>
-  <GoalForm/>
-  <section className="content">
-    {goals.length > 0 ? (
-      <div className='goals'>
-        {goals.map((goal) => (
-          <GoalItem key={goal._id} goal={goal}/>
-        ))}
-      </div>
-    ):(
-      <h3>You have not set any goals </h3>
-    )}
-  </section>
+    <section className="heading">
+      <h1>Welcome {user && user.name}</h1>
+      <p>Goals Dashboard</p>
+    </section>
+    <GoalForm/>
+    <section className="content">
+      {goals.length > 0 ? (
+        <div className='goals'>
+          {goals.map((goal) => (
+              <GoalItem key={goal._id} goal={goal}/>     
+          ))}
+        </div>
+      ):(
+        <h3>You have not set any goals </h3>
+      )}
+    </section>
   </>
-  
-}
+};
 
 export default Dashboard
