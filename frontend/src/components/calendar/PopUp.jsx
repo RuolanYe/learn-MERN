c92c9ccfc5213d23dfbcd2c05d3a8127b25c1957
Sophia Ye <sophiaye@Sophias-Air-3.hitronhub.home>
2022-07-05 16:49:56 -0700

    remove scroll in popup

--- frontend/src/components/calendar/PopUp.jsx
+++ frontend/src/components/calendar/PopUp.jsx
@@ -1,23 +1,27 @@
 import React, {FC} from 'react'
-import {Dialog, DialogTitle, TextField, Button, DialogContent, DialogActions,Typography,Card,CardMedia} from '@material-ui/core'
+import {  } from '@material-ui/core/Dialog';
+import {Dialog, DialogTitle, TextField, Button, DialogContent, DialogContentText,Typography,Card,CardMedia} from '@material-ui/core'
 import {useDispatch, useEffect} from 'react-redux'
 import {deleteGoal} from'../../features/goals/goalSlice'
+import './styles.css'
 
 
 // interface Props{
 //     handleClose:()=>any;
 //     clickedEvent
 // }
+// const [scroll, setScroll] = React.useState <DialogProps['scroll']> ('body');
+// setScroll(DialogProps['scroll']);
 
 function PopUp (Props) {
   const dispatch = useDispatch()
   // console.log(Props.clickedEvent.extendedProps.imageurl)
   // console.log(Props.clickedEvent.id)
 
-  return <Dialog open={true} onClose={Props.handleClose}>
+  return <Dialog open={true} onClose={Props.handleClose} scroll={'body'}>
     <DialogTitle>{Props.clickedEvent.title}</DialogTitle>
     <DialogContent>{Props.clickedEvent.start.toLocaleString('en-US')}</DialogContent>
-    <CardMedia component="img" image={Props.clickedEvent.extendedProps.imageurl}/>
+    <CardMedia className="popUpImage" component="img" image={Props.clickedEvent.extendedProps.imageurl}/>
     <Button onClick={() => dispatch (deleteGoal(Props.clickedEvent.id)) }>Delete</Button>
     <Button onClick={Props.handleClose}>Close</Button>
     {/* <CardMedia component="img" image={Props.clickedEvent.articleImage}/> */}
